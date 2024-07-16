32e71f5d1f9e225936ff16de19f04e3adf6640e9
Ben Klinovsky <bnk989@gmail.com>
2024-07-16 19:08:27 +0300

    Add topNav for admin order (ui only)

---
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Tabs, withLayoutContext } from "expo-router";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator);
+
+export default function OrderListNavigator() {
+    return (
+        <SafeAreaView edges={["top"]} style={{ flex: 1 }}>
+            <TopTabs >
+                <TopTabs.Screen name="index" options={{ title: 'Active' }} />
+                <TopTabs.Screen name="archive" options={{ title: 'Archive' }} />
+            </TopTabs>
+        </SafeAreaView>
+    );
+}
