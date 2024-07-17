8cf408f6b1100865bc7f46f0b14b40847c4bba30
Ben Klinovsky <bnk989@gmail.com>
2024-07-17 12:58:22 +0300

    implement first api route with custom hook

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -2,22 +2,14 @@ import Colors from '@/src/constants/Colors'
 import { ActivityIndicator, FlatList, StyleSheet, Text } from 'react-native'
 // import products from '@/assets/data/products'
 import ProductListItem from '@/src/components/ProductListItem'
-import { useEffect } from 'react'
-import { supabase } from '@/src/lib/supabase'
-import { useQuery } from '@tanstack/react-query'
+import { useProductList } from '@/src/api/products'
+
 
 
 
 export default function MenuScreen() {
 
-  const { data: products, error, isLoading } = useQuery({
-    queryKey: ['products'],
-    queryFn: async () => {
-      const {data, error} = await supabase.from('products').select('*')
-      if(error) throw new Error(error.message)
-      return data
-    }
-  })
+  const { data: products, error, isLoading } = useProductList()
 
   if(isLoading) return <ActivityIndicator size="small" color="#0000ff" />
   if(error) return <Text> error while loading products</Text>
