4bcb431a90ed861e6f1341c18c9e2f6e09cb0490
Ben Klinovsky <bnk989@gmail.com>
2024-07-19 17:58:19 +0300

    Add manual refresh function to user orders

--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -1,19 +1,31 @@
 import OrderListItem from '@/components/OrderListItem'
 import { useUserOrderList } from '@/src/api/orders'
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
+import {
+    ActivityIndicator,
+    FlatList,
+    RefreshControl,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native'
 
 export default function OrdersScreen() {
-    const { data: orders, error, isLoading } = useUserOrderList()
+    const { data: orders, error, isLoading, refetch, isRefetching } = useUserOrderList()
+
     if (isLoading) return <ActivityIndicator size="large" color="#ddd" />
     if (error) return <Text>error while loading orders</Text>
 
     return (
         <View style={styles.container}>
+            {isRefetching && <ActivityIndicator size="large" color="#ddd" />}
             <FlatList
                 style={styles.list}
                 data={orders}
                 contentContainerStyle={{ gap: 10, padding: 10 }}
                 renderItem={({ item }) => <OrderListItem order={item} />}
+                refreshControl={
+                    <RefreshControl refreshing={isLoading} onRefresh={refetch} />
+                }
             />
         </View>
     )
